71a6de63c873675db319bc078639cfc0ca861303
mbageri <mohammadbageri@outlook.com>
2023-09-30 00:57:26 +0200

    add count

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ resource "aws_instance" "web-server" {
   ami           = var.ami_id
   instance_type = var.instance_type
   key_name      = var.key_name
-
+  count = 3
   tags = {
-    Name = var.instance_name
+    Name = "machine${count.index}"
   }
 }
