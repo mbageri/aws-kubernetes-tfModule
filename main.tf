ee0f0f7e035a66814ab41af56494b88d50d4a531
mbageri <mohammadbageri@outlook.com>
2023-09-30 01:49:52 +0200

    detaching the module

--- main.tf
+++ main.tf
@@ -1,10 +1,16 @@
+module "ec2" {
+  # source = "git@github.com:mbageri/ec2_module.git"
+  source = "git@github.com:mbageri/ec2_module.git"
 
-resource "aws_instance" "web-server" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name      = var.key_name
-
-  tags = {
-    Name = v
-  }
 }
+
+
+# resource "aws_instance" "web-server" {
+#   ami           = var.ami_id
+#   instance_type = var.instance_type
+#   key_name      = var.key_name
+
+#   tags = {
+#     Name = v
+#   }
+# }
