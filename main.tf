5fb49d6b94f1d790e48d31efd5aa559c5e0cba94
mbageri <mohammadbageri@outlook.com>
2023-09-30 01:58:49 +0200

    cleaning code

--- main.tf
+++ main.tf
@@ -3,14 +3,3 @@ module "ec2" {
   source = "git@github.com:mbageri/ec2_module.git"
 
 }
-
-
-# resource "aws_instance" "web-server" {
-#   ami           = var.ami_id
-#   instance_type = var.instance_type
-#   key_name      = var.key_name
-
-#   tags = {
-#     Name = v
-#   }
-# }
