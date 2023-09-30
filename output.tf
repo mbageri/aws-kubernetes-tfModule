95da9843d3b19098d025b4926d4f8a8d8eddc121
mbageri <mohammadbageri@outlook.com>
2023-10-01 01:42:14 +0200

    update

--- output.tf
+++ output.tf
@@ -1,16 +1,3 @@
-# output "public_ip" {
-#    value = aws_instance.web-server[*].public_ip
-#    value = ["${aws_instance..public_ip}"]
-
-# }
-
 output "public_ip" {
-  description = "its ip public"
-  value = try(
-    aws_instance.web-server[0].public_ip,
-    aws_instance.web-server[1].public_ip,
-    aws_instance.web-server[2].public_ip,
-    aws_instance.web-server[3].public_ip,
-    null,
-  )
+    value = join(aws_instance.web-server[*].ipv4_address)
 }
